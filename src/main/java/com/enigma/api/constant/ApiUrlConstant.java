0400f0b33b92098d546aece8602123f97b03cc97
tika.kristiani <tika.kristiani@gmail.com>
2021-02-22 17:59:07 +0700

    [IMPROVE]constants to product path

---
+++ src/main/java/com/enigma/api/constant/ApiUrlConstant.java
@@ -0,0 +1,5 @@
+package com.enigma.api.constant;
+
+public class ApiUrlConstant {
+    public static final String PRODUCT = "/products";
+}
--- src/main/java/com/enigma/api/controller/ProductController.java
+++ src/main/java/com/enigma/api/controller/ProductController.java
@@ -1,5 +1,6 @@
 package com.enigma.api.controller;
 
+import com.enigma.api.constant.ApiUrlConstant;
 import com.enigma.api.entity.Product;
 import com.enigma.api.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,38 +12,39 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping(ApiUrlConstant.PRODUCT)
 public class ProductController {
     @Autowired
     ProductService productService;
 
-    @GetMapping("/product/{id}")
+    @GetMapping("/{id}")
     public Product getProductById(@PathVariable String id){
         return productService.getProductById(id);
     }
 
-    @PostMapping("/product")
+    @PostMapping
     public Product saveProduct(@RequestBody Product product){
         return productService.saveProduct(product);
     }
 
-    @GetMapping("/products")
+    @GetMapping
     public List<Product> allProducts(){
         return productService.getAllProduct();
     }
 
-    @GetMapping("/productsPage")
+    @GetMapping("/page")
     public Page<Product> productsPerPage(@RequestParam(name = "page", defaultValue = "0") Integer page,
                                          @RequestParam(name = "size", defaultValue = "10") Integer sizePerPage){
         Pageable pageable = PageRequest.of(page, sizePerPage);
         return productService.getProductsByPage(pageable);
     }
 
-    @DeleteMapping("/product")
+    @DeleteMapping
     public void deleteProduct(@RequestParam(name = "id") String id){
         productService.deleteProduct(id);
     }
 
-    @PutMapping("/product")
+    @PutMapping
     public Product editProduct(@RequestBody Product product){
         return productService.editProduct(product);
     }
