ded6829e38387049b37106b3e6c69096331e4053
tika.kristiani <tika.kristiani@gmail.com>
2021-03-22 14:13:13 +0700

    [FIX] Data created exception

---
+++ src/main/java/com/enigma/api/exception/DataCreated.java
@@ -0,0 +1,12 @@
+package com.enigma.api.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CREATED)
+public class DataCreated extends RuntimeException{
+    public static final String CREATED_MESSAGE = "Transaction with purchase id %s created";
+    public DataCreated(String message){
+        super(message);
+    }
+}
